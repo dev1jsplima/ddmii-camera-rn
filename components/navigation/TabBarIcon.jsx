69a27b212c504eade12af176e960051cca6ebc4c
dev1jsplima <148554763+dev1jsplima@users.noreply.github.com>
2025-09-19 10:51:22 -0300

    Update TabBarIcon.jsx

--- components/navigation/TabBarIcon.jsx
+++ components/navigation/TabBarIcon.jsx
@@ -1,5 +1,7 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export function TabBarIcon({ style, ...rest }) {
-    return <Ionicons size={28} style={[{marginBottom :-3}, style]} {...rest} />;
+    return <Ionicons size={28} style={[{marginBottom :-3}, style] 
+    {...rest}
+    } />;
 }
